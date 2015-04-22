683c1dc10a12d719ad8cfdea581366d6d7c1e001
Marco 2 <m.bonera128@gmail.com>
2015-04-22 17:44:12 +0200

    boh

--- tamagochi/src/tamagochi/TamagochiMain.java
+++ tamagochi/src/tamagochi/TamagochiMain.java
@@ -5,29 +5,94 @@ public class TamagochiMain
 	public static void main (String[] args)
 	
 	{
-		char comando;
-		comando='a';
 		
-		int azione;
-		azione=0;
-		int quantita;
-		quantita=0;
+		// INIZIALIZZAZIONE
 		
-		Tamagochi tama1 = new Tamagochi("pino", 25, 50);
+		char comando;			// comando dell'utente
+		int azione;				// cosa fare al tamagochi
+		int quantita_tamagochi;		// quanti tamagochi usare
+		int quantita_azioni;		//quante cose fare al tamagochi
+		Tamagochi vettore [];	// lo zoo
+		int i;					//contatore
+		String nome;			// il nome del tamagochi
 		
-		System.out.println("Gioco del tamagochi:\n E - Esci\n M - Dai da mangiare\n A - Accarezza\n");
-		System.out.println ("ora il tamagochi " + tama1.come_sta_stringa_debug());
-		System.out.println ();
 		
-		while (((comando!='Q') || (comando!='q')) && (tama1.get_stato()>0))
+		// SALUTO
+		
+		System.out.println("Gioco del tamagochi\n\n");
+		System.out.println("quanti tamagochi si vogliono creare?");
+		
+		// ricevi quantita
+		quantita_tamagochi=3;
+		
+		
+		// CREAZIONE DEI TAMAGOCHI
+		
+		vettore = new Tamagochi [quantita_tamagochi];
+		
+		for (i=0; i<quantita_tamagochi; i++)
+		
 		{
-			
-			quantita=5;
-			
-			tama1.accudisci(azione, quantita);
-			System.out.print ("ora il tamagochi " + tama1.come_sta_stringa());
-			System.out.println ("   ---   " + tama1.come_sta_stringa_debug());
+			System.out.print("inserire il nome del " + i + "^ tamagochi: ");
+			//ricevi nome
+			nome = "pino";
+			vettore [i] = new Tamagochi (nome);
 		}
+		
+		
+		// INIZIO GIOCO
+		
+		do
+		{
+			System.out.println("E - Esci\n M - Dai da mangiare\n A - Accarezza\n\n");
+			// ricevi comando
+			comando='a';
+			
+			if ((comando=='Q') || (comando=='q'))
+			{
+				
+			}
+			
+			if ((comando=='M') || (comando=='m'))
+			{
+				azione=0;
+			}
+			
+			if ((comando=='A') || (comando=='a'))
+			{
+				azione=1;
+			}
+			
+			
+			
+			azione=0;
+			quantita_azioni=5;
+			
+			for (i=0; i<quantita_tamagochi; i++)
+			{
+				if (vettore[i].get_stato()>0)
+				{
+					vettore[i].accudisci(azione, quantita_azioni);
+				}
+			}
+			
+			for (i=0; i<quantita_tamagochi; i++)
+			{
+				System.out.print ("ora il tamagochi " + i + " " + vettore[i].come_sta_stringa() );
+				System.out.print ("ora il tamagochi " + vettore[i].come_sta_stringa_debug() );
+			}
+			
+			
+			for (i=0; i<quantita_tamagochi; i++)
+			{
+				// metti l'uscita dal morto
+			}
+			
+		}
+		while ((comando!='E') || (comando!='e'));
+		
+		System.out.println("tutti morti");
+		
 	}
 	
 
